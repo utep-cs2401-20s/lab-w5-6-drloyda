54aac7ee69ef233a9660b6825f9082bb1d8d24db
drloyda <drloyda@miners.utep.edu>
2020-02-22 11:44:44 -0700

    added exhaustive method, print method ,neighbors method and tester class

--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -3,12 +3,14 @@ public class SnakeGame {
     private int [] headPosition;
     private static int exhaustiveChecks;
     private static int recursiveChecks;
+    private int [] result; // i made to print the result
 
-    //Default constructor
+    //Default constructor assigns game board of one element
     SnakeGame(){
         game = new boolean[1][1];
     }
 
+    //Constructor that initializes gameboard and headPosition
     SnakeGame(boolean[][] board, int x, int y){
         game = new boolean[board.length][board.length];
         headPosition = new int[2];
@@ -31,43 +33,103 @@ public class SnakeGame {
         //if two or more neighbors then move to next cell
         //return array[3] (tail position [x,y] and length
         resetCounters();
+        int tailRow = 0;
+        int tailCol = 0;
+        int length = 0;
 
         for (int i = 0; i < game.length; i++) {
             for (int j = 0; j < game[i].length; j++) {
-                if (game[i][j]) {)
-                    if (neighbors(game,i,j) == 1 && (game[i] = headPosition[0]) && (game[j] = headPosition[1])){
+                exhaustiveChecks++;
+                if (game[i][j]){
+                    length++;
+                    if (neighbors(game,i,j) == 1 && (game[i] != game[headPosition[0]]) && (game[i] != game[headPosition[0]])  ){
+                        tailRow = i;
+                        tailCol = j;
                     }
-
                 }
             }
         }
-        return
+        result = new int [] {tailRow,tailCol,length};
+        return result;
     }
-    public int neighbors(boolean [][] game, int row, int col){
-        int neighbors = 0;
-        if(game[row][col +1])//checks right
-            neighbors++;
-        if(game[row][col -1])//checks left
-            neighbors++;
-        if(game[row + 1][col])//checks down
-            neighbors++;
-        if(game[row -1][col])//checks up
-            neighbors++;
-
-        return neighbors;
+//    public int [] findTailRecursive(){
+//        if(game[headPosition[0] + 1][headPosition[1]])
+//
+//
+//    }
+    public void print(){
+        System.out.println("Head position: (" + headPosition[0] + ", " + headPosition[1] + ")");
+        System.out.println("Tail position: (" + result[0] + ", " + result[1] + ")");
+        System.out.println("Length: " + result[2]);
+        System.out.println("Exhaustive Checks: " + exhaustiveChecks);
     }
 
     private void resetCounters(){
         exhaustiveChecks = 0;
         recursiveChecks = 0;
-
     }
+
     private static int getExhaustiveChecks(){
         return exhaustiveChecks;
     }
+
     private static int getRecursiveChecks(){
         return recursiveChecks;
     }
+
+    public int neighbors(boolean [][] game, int row, int col){
+//        boolean right = (game[row][col +1]);
+//        boolean left = (game[row][col -1]);
+//        boolean down = (game[row + 1][col]);
+//        boolean up = (game[row -1][col]);
+
+        int neighbors = 0;
+
+        if(row == 0 && col == 0){
+            if(game[row][col +1])
+                neighbors++;
+            if(game[row + 1][col])
+                neighbors++;
+        }
+
+        if(row == game.length-1 && col == game.length -1){
+            if((game[row][col -1]))
+                neighbors++;
+            if(game[row -1][col])
+                neighbors++;
+        }
+
+        if(row == 0 && col != game.length-1){
+            if((game[row][col -1]))
+                neighbors++;
+            if(game[row][col +1])
+                neighbors++;
+            if(game[row + 1][col])
+                neighbors++;
+        }
+        if(col == 0 && row != game.length-1 ){
+            if(game[row][col +1])
+                neighbors++;
+            if((game[row - 1][col]))
+                neighbors++;
+            if(game[row + 1][col])
+                neighbors++;
+        }
+
+        if(row != 0 && row != game.length-1 && col != 0 && col!= game.length-1){
+            if(game[row][col +1])
+                neighbors++;
+            if((game[row][col -1]))
+                neighbors++;
+            if(game[row + 1][col])
+                neighbors++;
+            if(game[row -1][col])
+                neighbors++;
+        }
+        return neighbors;
+    }
+
+
 
 
 }
---
+++ java/src/SnakeGameTester.java
@@ -0,0 +1,18 @@
+public class SnakeGameTester {
+    public static void main(String[] args) {
+        boolean[][] test = new boolean[][]{ {false, false, false, false, false, false, false, false},
+                                            {false, false, false, false, false, false, false, false},
+                                            {true, true, true, true, true, false, false, false},
+                                            {true, false, false, false, true, false, false, false},
+                                            {true, false, false, false, true, false, false, false},
+                                            {true, false, false, false, true, false, false, false},
+                                            {true, false, false, false, false, false, false, false},
+                                            {true, false, false, false, false, false, false, false},};
+        SnakeGame game = new SnakeGame(test,7,0);
+        game.findTailExhaustive();
+        game.findTailExhaustive();
+        game.print();
+
+
+    }
+}
