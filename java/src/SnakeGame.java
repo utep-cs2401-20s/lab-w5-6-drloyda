aa0579915f6fb4337d65375cf656bf04616a5b6a
drloyda <drloyda@miners.utep.edu>
2020-02-25 21:32:45 -0700

    added more to findTailRecursive method

--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -23,6 +23,7 @@ public class SnakeGame {
             }
         }
     }
+    //---------------------------------------------------------------------------------------------------------------
     public int[] findTailExhaustive() {
         //reset counter
         //increases check counter each time
@@ -52,31 +53,55 @@ public class SnakeGame {
         result = new int [] {tailRow,tailCol,length};
         return result;
     }
+    //---------------------------------------------------------------------------------------------------------------
 //    public int [] findTailRecursive(){
-//        if(game[headPosition[0] + 1][headPosition[1]])
+//       // result = new int [] {tailRow,tailCol,length};
 //
+//        findTailRecursive(headPosition,headPosition);
 //
+//        return result;
 //    }
+    //---------------------------------------------------------------------------------------------------------------
+    private int [] findTailRecursive(int [] currentPosition, int [] previousPosition){
+        int [] temp = new int[2];
+        int [] next = new int[2];
+        temp[0] = currentPosition[0];
+        temp[1] = currentPosition[1];
+
+        neighborsRec(game, currentPosition[0], currentPosition[1], previousPosition, next);
+        length++;
+
+        currentPosition[0] = next[0];
+        currentPosition[1] = next [1];
+
+        previousPosition [0] = temp[0];
+        previousPosition [1] = temp[1];
+
+        findTailRecursive(currentPosition,previousPosition);
+
+        return result = new int [] {currentPosition[0],currentPosition[1],length};
+    }
+    //---------------------------------------------------------------------------------------------------------------
     public void print(){
         System.out.println("Head position: (" + headPosition[0] + ", " + headPosition[1] + ")");
         System.out.println("Tail position: (" + result[0] + ", " + result[1] + ")");
         System.out.println("Length: " + result[2]);
         System.out.println("Exhaustive Checks: " + exhaustiveChecks);
     }
-
+    //---------------------------------------------------------------------------------------------------------------
     private void resetCounters(){
         exhaustiveChecks = 0;
         recursiveChecks = 0;
     }
-
+    //---------------------------------------------------------------------------------------------------------------
     private static int getExhaustiveChecks(){
         return exhaustiveChecks;
     }
-
+    //---------------------------------------------------------------------------------------------------------------
     private static int getRecursiveChecks(){
         return recursiveChecks;
     }
-
+    //---------------------------------------------------------------------------------------------------------------
     public int neighbors(boolean [][] game, int row, int col){
 //        boolean right = (game[row][col +1]);
 //        boolean left = (game[row][col -1]);
@@ -127,6 +152,91 @@ public class SnakeGame {
                 neighbors++;
         }
         return neighbors;
+    }
+    //---------------------------------------------------------------------------------------------------------------
+    public int [] neighborsRec(boolean [][] game, int row, int col, int [] prev, int [] next){
+//        boolean right = (game[row][col +1]);
+//        boolean left = (game[row][col -1]);
+//        boolean down = (game[row + 1][col]);
+//        boolean up = (game[row -1][col]);
+
+
+
+        if(row == 0 && col == 0){
+            if(game[row][col +1] && (game[row] != game[prev[0]]) && (game[col+1] != game[prev[1]])){
+                next[0] = row;
+                next[1] = col +1;
+            }
+            if(game[row + 1][col] && (game[row + 1] != game[prev[0]]) && (game[col] != game[prev[1]])){
+                next[0]= row + 1;
+                next[1] = col;
+            }
+
+        }
+
+        if(row == game.length-1 && col == game.length -1){
+            if(game[row][col -1] && (game[row] != game[prev[0]]) && (game[col-1] != game[prev[1]])){
+                next[0]= row;
+                next[1] = col -1;
+            }
+
+            if(game[row -1][col] && (game[row-1] != game[prev[0]]) && (game[col] != game[prev[1]])){
+                next[0]= row - 1;
+                next[1] = col;
+            }
+        }
+
+        if(row == 0 && col != game.length-1){
+            if((game[row][col -1]) && (game[row] != game[prev[0]]) && (game[col-1] != game[prev[1]])){
+                next[0]= row;
+                next[1] = col -1;
+            }
+            if(game[row][col +1] && (game[row] != game[prev[0]]) && (game[col+1] != game[prev[1]])){
+                next[0]= row;
+                next[1] = col + 1;
+            }
+            if(game[row + 1][col] && (game[row+1] != game[prev[0]]) && (game[col] != game[prev[1]])){
+                next[0]= row + 1;
+                next[1] = col;
+            }
+        }
+        if(col == 0 && row != game.length-1 ){
+            if(game[row][col +1] && (game[row] != game[prev[0]]) && (game[col+1] != game[prev[1]])){
+                next[0]= row;
+                next[1] = col + 1;
+            }
+
+            if(game[row -1][col] && (game[row-1] != game[prev[0]]) && (game[col] != game[prev[1]])){
+                next[0]= row - 1;
+                next[1] = col;
+            }
+            if(game[row + 1][col] && (game[row+1] != game[prev[0]]) && (game[col] != game[prev[1]])){
+                next[0]= row + 1;
+                next[1] = col;
+            }
+
+        }
+
+        if(row != 0 && row != game.length-1 && col != 0 && col!= game.length-1){
+            if(game[row][col +1] && (game[row] != game[prev[0]]) && (game[col+1] != game[prev[1]])){
+                next[0]= row;
+                next[1] = col + 1;
+            }
+
+            if((game[row][col -1]) && (game[row] != game[prev[0]]) && (game[col-1] != game[prev[1]])){
+                next[0]= row;
+                next[1] = col -1;
+            }
+            if(game[row + 1][col] && (game[row+1] != game[prev[0]]) && (game[col] != game[prev[1]])){
+                next[0]= row + 1;
+                next[1] = col;
+            }
+            if(game[row -1][col] && (game[row-1] != game[prev[0]]) && (game[col] != game[prev[1]])){
+                next[0]= row -1;
+                next[1] = col;
+            }
+        }
+        return next;
     }
 
 
